6a15abbb58274d3a50de170113a6a340f50e08c3
sebasdeldi <sebasdeldi123@gmail.com>
2015-12-29 17:58:12 -0500

    Style added

--- data_base_mng.rb
+++ data_base_mng.rb
@@ -1,16 +1,12 @@
-require 'sinatra'
-require 'data_mapper'
-
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/notes_app.db")
 
 class Note
-	include DataMapper::Resource
-	property :id, Serial
-	property :content, Text, :required => true
-	property :complete, Boolean, :required => true, Default => false
-	property :created_at, DateTime
-	property :updated_at, DateTime
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :complete, Boolean, :required => true, :default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
 end
 
-DataMapper.auto_upgrade!
-
+DataMapper.finalize.auto_upgrade!
--- routes.rb
+++ routes.rb
@@ -1,7 +1,25 @@
+require 'data_mapper'
+require 'sinatra'
+
 require_relative ("./data_base_mng.rb")
 
 get '/' do
-	@notes = Note.all :order => :id.desc
-	@title = "All Notes"
-	erb :home
+    @notes = Note.all :order => :id.desc
+    @title = "All Notes"
+    erb :home
+end
+
+post '/' do
+	n = Note.new
+	n.content = params[:content]
+	n.created_at = Time.now
+	n.updated_at = Time.now
+	n.save
+	redirect '/'
+end
+
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
 end
